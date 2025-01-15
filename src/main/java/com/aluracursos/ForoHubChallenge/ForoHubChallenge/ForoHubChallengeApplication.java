aec13b1d60a4945961d01ed55202e4e7531631f5
Harold Cortes <harcor59p@gmail.com>
2025-01-15 08:27:40 -0500

    first commit

---
+++ src/main/java/com/aluracursos/ForoHubChallenge/ForoHubChallenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.ForoHubChallenge.ForoHubChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
